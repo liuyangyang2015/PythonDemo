885da601ac209ab18c5747129c5973d7521df8b9
liuyangyang <liuyangyang@banmatrip.com>
2018-01-15 10:18:14 +0800

    ''

---
+++ com/banma/demo/test2.py
@@ -0,0 +1,87 @@
+# a=4|3
+# print(a)
+
+# coding: UTF-8
+
+# month_names = ['Januari', 'Februari', 'Maart',      # These are the
+#  'April',   'Mei',      'Juni',       # Dutch names
+#                'Juli',    'Augustus', 'September',  # for the months
+#                'Oktober', 'November', 'December']   # of the year
+# print(month_names)
+
+# 哈 = "hello" 'world''hj'
+# print(哈)
+# re.compile("[A-Za-z_]"       # letter or underscore
+#            "[A-Za-z0-9_]*"   # letter, digit or underscore
+#           )
+
+name = 'f'
+# a= f'he said his name is {name!r}'
+# print(a)
+# a = f'he said his name is {(name)!a}'
+a= 0e0
+# a= a + 1
+# print(a)
+# b = 3.14_15_93
+# print(b)
+
+# a = 3.14j
+# b=1
+# c =a+b
+# print(c)
+
+# class a(object):
+#     name ='hi'
+#     def say(self):
+#         print(name)
+#
+# b= a()
+# print(b.name)
+# c=id(b)
+# print(c)
+# print
+# def say():
+#     pass
+#
+# print(say())
+# a = NotImplemented
+# print(a)
+#
+# a= '2'
+# print(ord(a))
+# print(chr(50))
+#
+# a= "123"
+# print(str.encode(a))
+# p=str.encode(a)
+# print(type(p))
+# print(bytes.decode(p))
+# m = bytes.decode(p)
+# print(type(m))
+# def say():
+#     print('hi')
+# a=say
+# print(a())
+#
+# class a(object):
+#     name ='hi'
+#     def say(self):
+#         print(name)
+#
+# b= a()
+# print(b.__class_())
+# import sys
+# sys.stdout("hi")
+
+# def _new_():
+#     print("hi")
+#
+#
+# class a(object):
+#     name ='hi'
+#     def say(self):
+#         print(name)
+#
+# b= a()
+# print(b.__class_())
+
---
+++ com/banma/demo/test3.py
@@ -0,0 +1,8 @@
+print(0x018DC410)
+print(26067984)
+a2=141
+b2=112
+c2=151
+print(id(a2))
+print(id(b2))
+print(id(c2))
