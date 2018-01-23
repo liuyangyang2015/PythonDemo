e02f7860c12865aeafa01db28c44451a29efaa87
liuyangyang <liuyangyang@banmatrip.com>
2018-01-23 11:51:11 +0800

    'demo:add'

---
+++ com/banma/test/test4.py
@@ -0,0 +1,12 @@
+class Student(object):
+
+    def __init__(self, name, score):
+        self.__name = name
+        self.__score = score
+
+    def print_score(self):
+        print('%s: %s' % (self.__name, self.__score))
+
+bart = Student('Bart Simpson', 59)
+print(bart._Student__name)
+bart.__name
