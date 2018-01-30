2ac44ea063f12bcfa96143a8c8df509a49e5072d
liuyangyang <liuyangyang@banmatrip.com>
2018-01-30 23:40:31 +0800

    'log'

--- com/bigroad/python/demo/test2.py
+++ com/bigroad/python/demo/test2.py
@@ -1,9 +1,10 @@
 # from com.demo import dict
 import logging
 # from logging.config import fileConfig
+logger = logging.getLogger('com.bigroad.python.demo.test2')
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',filename='example.log',level=logging.DEBUG)
 # logging.getLogger(__name__).addHandler(logging.NullHandler())
 print(__name__)
 print('hi')
 logging.warning('hihi')
-logging.info('watch out')
+logger.info('watch out')
