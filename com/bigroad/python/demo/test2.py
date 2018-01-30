413c979ccdbd04399b44569c5f8224ff2b0505eb
liuyangyang <liuyangyang@banmatrip.com>
2018-01-30 23:35:45 +0800

    'log'

--- com/bigroad/python/demo/test2.py
+++ com/bigroad/python/demo/test2.py
@@ -1,7 +1,7 @@
 # from com.demo import dict
 import logging
 # from logging.config import fileConfig
-logging.basicConfig(filename='example.log',level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',filename='example.log',level=logging.DEBUG)
 # logging.getLogger(__name__).addHandler(logging.NullHandler())
 print(__name__)
 print('hi')
