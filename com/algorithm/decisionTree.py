a72c009a31ff833dd12405bb97e688ae91ceda6c
liuyangyang2015 <liuyangyang_2010@126.com>
2018-02-25 23:02:46 +0800

    dev: shannonEnt

--- com/algorithm/decisionTree.py
+++ com/algorithm/decisionTree.py
@@ -11,3 +11,28 @@ def createDataSet():
 
 # 计算给定数据集的香农熵
 
+from math import log
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] +=1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        print(labelCounts[key]/numEntries)
+        prob = float(labelCounts[key]/numEntries)
+        shannonEnt -= prob*log(prob,2)
+    return shannonEnt
+
+# 测试熵计算
+def testCalcShannnonEnt(list):
+    num = sum(list)
+    shannonEnt = 0
+    for entry in list:
+        prop = entry/num
+        shannonEnt -= prop*log(prop,2)
+    return shannonEnt
+
--- com/algorithm/test2.py
+++ com/algorithm/test2.py
@@ -3,3 +3,20 @@ dataSet,labels = decisionTree.createDataSet()
 print(dataSet)
 print(labels)
 
+shannonEnt = decisionTree.calcShannonEnt(dataSet)
+print(shannonEnt)
+
+test = decisionTree.testCalcShannnonEnt([3,3])
+test2 = decisionTree.testCalcShannnonEnt([2,4])
+test3 = decisionTree.testCalcShannnonEnt([1,1,4])
+print(test,":",test2,":",test3)
+
+from pylab import *
+
+X = np.linspace(-np.pi, np.pi, 256,endpoint=True)
+C,S = np.cos(X), np.sin(X)
+
+plot(X,C)
+plot(X,S)
+
+show()
